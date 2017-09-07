c8425cfc91767b65d3ca1ed9a171376990fc570d
alancheg <chenggui53@gmail.com>
2017-09-07 09:23:10 +0800

    pca 增加文件

--- word_pca.py
+++ word_pca.py
@@ -16,8 +16,16 @@ import os
 
 if __name__ == "__main__":
 
-    if os.path.exists('word2d.dict'):
-        pass
+
+    # ---- 产生降维的权重 ---- #
+    if os.path.exists('word2d.list') or os.path.exists('word2d.weight'):
+        # ---- 如果相关的权重已经生成完成 ---- #
+        with open(r'D:\desktop\newrtree\word2d.list', 'rb') as list_file:
+            word_list = list(pickle.load(list_file))
+
+        with open(r'D:\desktop\newrtree\word2d.weight', 'rb') as weight_file:
+            word_weight = list(pickle.load(weight_file))
+
     else:
         # ---- 首先基于中文词库生成对应的词向量 ---- #
 
@@ -41,7 +49,7 @@ if __name__ == "__main__":
         new_word_weight = pca_generator.fit_transform(word_list_weight)
         print(new_word_weight)
 
-        # 将词和对应的降维向量合并到词典类型中，并且进行持久化存储 ---- #
+        # ---- 将词和对应的降维向量合并到词典类型中，并且进行持久化存储 ---- #
         with open('word2d.list', 'wb') as output:
             pickle.dump(word_list, output)
             print('list saved to word2d.list')
@@ -50,16 +58,3 @@ if __name__ == "__main__":
             pickle.dump(new_word_weight, output)
             print('weight saved to word2d.weight')
 
-
-        # word_2d_dict = {}
-        # for i in range(len(word_list)):
-        #     word_2d_dict[word_list[i]] = new_word_weight[i]
-        #
-        # # 使用 pickle 存储数据字典 #
-        # with open('word2d.dict', 'wb', encoding='utf-8') as output:
-        #     pickle.dump(word_2d_dict, output)
-
-
-    # ---- 对词向量进行降维处理，从而转换成二维数据 ---- #
-
-    # ---- 将词向量结合到树中 ---- #
